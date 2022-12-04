ada409b12810651c24ec2caa5dab1b6779ebc21c
keyleeu <minhk.lee21@gmail.com>
2022-12-05 00:20:38 +0900

    #8

--- frontend/src/H_category/H_category.js
+++ frontend/src/H_category/H_category.js
@@ -16,24 +16,24 @@ function H_category() {
   }, []);
 
   return (
-    // <div>
-    //     {
-    //         Category.map(
-    //           t => {
-    //               return (
-    //                 <Categorylist Key={t.id} location={t.context} />
-    //               )
-    //           }
-    //         )
-    //     }
-    // </div>
     <div>
-      <Categorylist Key="1" location="뚜레주르" />
-      <Categorylist Key="2" location="봉마르쉐" />
-      <Categorylist Key="3" location="프린트실" />
-      <Categorylist Key="4" location="cu편의점" />
-      <Categorylist Key="5" location="호아토스트" />
+        {
+            Category.map(
+              t => {
+                  return (
+                    <Categorylist Key={t.id} location={t.context} />
+                  )
+              }
+            )
+        }
     </div>
+    // <div>
+    //   <Categorylist Key="1" location="뚜레주르" />
+    //   <Categorylist Key="2" location="봉마르쉐" />
+    //   <Categorylist Key="3" location="프린트실" />
+    //   <Categorylist Key="4" location="cu편의점" />
+    //   <Categorylist Key="5" location="호아토스트" />
+    // </div>
   );
 }
 
